0f06c84e85d1087e6f7bf73e949baee0925075b3
OnurKader <onurkader@protonmail.com>
2020-01-10 23:17:28 +0000

    Updated Makefile a bit more

--- src/main.c
+++ src/main.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define STB_IMAGE_IMPLEMENTATION
+#include "stb_image.h"
 
 int main(int argc, char** argv)
 {
+	printf("Hello There!\n\tGeneral Kenobi, you're a bold one!\n");
 	return 0;
 }
