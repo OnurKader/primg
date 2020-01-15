5b922bf05b5dc074a8df0153e2d58c4852784cc3
OnurKader <onurkader@protonmail.com>
2020-01-15 16:06:39 +0000

    Intermediate update, basics done. > Added color and some simple printing

---
+++ src/color.h
@@ -0,0 +1,64 @@
+#ifndef COLOR_H
+#define COLOR_H
+
+#include <stdint.h>
+#include <stdio.h>
+
+typedef struct color_t
+{
+	uint8_t fr, fg, fb;
+	uint8_t br, bg, bb;
+} color_t;
+
+void setColor(color_t* color, uint8_t r, uint8_t g, uint8_t b)
+{
+	color->fr = r;
+	color->fg = g;
+	color->fb = b;
+
+	color->br = r;
+	color->bg = g;
+	color->bb = b;
+}
+
+void setColorP(color_t* color, uint8_t* p)
+{
+	color->fr = p[0];
+	color->fg = p[1];
+	color->fb = p[2];
+
+	color->br = p[0];
+	color->bg = p[1];
+	color->bb = p[2];
+}
+
+void setRGB(
+	color_t* color, uint8_t fr, uint8_t fg, uint8_t fb, uint8_t br, uint8_t bg, uint8_t bb)
+{
+	color->fr = fr;
+	color->fg = fg;
+	color->fb = fb;
+
+	color->br = br;
+	color->bg = bg;
+	color->bb = bb;
+}
+
+void getStr(color_t* color, char* str)
+{
+	sprintf(str,
+			"\033[38;2;%hhu;%hhu;%hhum"
+			"\033[48;2;%hhu;%hhu;%hhum",
+			color->fr,
+			color->fg,
+			color->fb,
+
+			color->br,
+			color->bg,
+			color->bb);
+}
+
+const char RESET[] = "\033[38m\033[48m\033[m";
+
+#endif
+
--- src/main.c
+++ src/main.c
@@ -1,14 +1,27 @@
+#include "color.h"
+
 #include <getopt.h>
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <sys/ioctl.h>
 #include <unistd.h>
 
 #define STB_IMAGE_IMPLEMENTATION
 #include "stb_image.h"
 
 void usage(void);
+
+typedef struct TermSize
+{
+	uint16_t rows;
+	uint16_t cols;
+	uint16_t x_pixels;
+	uint16_t y_pixels;
+} TermSize;
+
+void getTermSize(TermSize*);
 
 int main(int argc, char** argv)
 {
@@ -23,7 +36,11 @@ int main(int argc, char** argv)
 	}
 
 	char filename[256] = "";
-	strcat(filename, argv[optind]);
+	if(argc > 1)
+		strcat(filename, argv[optind]);
+
+	TermSize ts;
+	getTermSize(&ts);
 
 	int width, height, chann;
 	unsigned char* img = stbi_load(filename, &width, &height, &chann, 0);
@@ -32,16 +49,54 @@ int main(int argc, char** argv)
 		fprintf(stderr, "\t\033[31;1mError while reading %s\033[m\n", filename);
 		exit(1);
 	}
-	size_t image_size = width * height * chann;
 
-	int greyscale = (chann == 1 || chann == 2);	   // Less than 3 <3 💜
-	for(const char* p = filename; *p < image_size; p += chann) {}
+	float img_aspect = (float)width / (float)height,
+		  term_aspect = (float)ts.x_pixels / (float)ts.y_pixels;
+
+	size_t image_size = width * height * chann;
 
 	printf("Image: %s\nWidth: %d\nHeight: %d\nNo. of channels: %d\n",
 		   filename,
 		   width,
 		   height,
 		   chann);
+
+	printf("rows: %hu\ncols: %hu\nx_pixels: %hu\ny_pixels: %hu\n",
+		   ts.rows,
+		   ts.cols,
+		   ts.x_pixels,
+		   ts.y_pixels);
+
+	const uint8_t greyscale = (chann == 1 || chann == 2);	 // Less than 3 <3 💜
+
+	if(img_aspect >= 1.f && term_aspect >= 1.f)
+	{
+		uint32_t n = 0U;
+		// TODO Handle image being smaller than terminal window
+		for(unsigned char* p = img; p < img + image_size; ++n)
+		{
+			color_t color;
+			char str[64];
+			setColorP(&color, p);
+			getStr(&color, str);
+			printf("%s\u2588", str);
+
+			// Skip the ratio of image to term
+			for(int i = 0; i < ceilf((float)width / (float)ts.x_pixels); ++i)
+				p += chann;
+			if(n % ts.cols == 0)
+				p += chann * ts.cols;
+		}
+	}
+
+	/* for(unsigned char* p = img; p != img + image_size; p += chann) */
+	/* { */
+	/* 	color_t color; */
+	/* 	char str[64]; */
+	/* 	setColorP(&color, p); */
+	/* 	getStr(&color, str); */
+	/* 	printf("%s\u2588", str); */
+	/* } */
 
 	stbi_image_free(img);
 	return 0;
@@ -53,3 +108,12 @@ void usage(void)
 	exit(1);
 }
 
+void getTermSize(TermSize* ts)
+{
+	struct winsize size;
+	ioctl(STDOUT_FILENO, TIOCGWINSZ, &size);
+	ts->rows = size.ws_row;
+	ts->cols = size.ws_col;
+	ts->x_pixels = size.ws_xpixel;
+	ts->y_pixels = size.ws_ypixel;
+}
